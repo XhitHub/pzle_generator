6be9d0af26c4eded16cd25c74c1e259e6b69fcb2
XhitHub <com.trojan.exe@gmail.com>
2021-06-07 12:29:45 +0800

    samples, dsgn

--- mechanics/tele.js
+++ mechanics/tele.js
@@ -5,6 +5,7 @@ class Tele {
     this.pzle = pzle
   }
 
+  // compulsory funcs
   forward() {
     // if any tele-able obj in pzle is in this.teleIn, tele it/them to teleOut
     this.pzle.items.forEach(
@@ -26,7 +27,22 @@ class Tele {
     )
   }
 
+  generatePrevStep(currState) {
+    // randomly create a mechanic of this type, with its attrs ics such that the pzle's prev step has its state results in currState with this mechanic instance involved
+    
+  }
+
+  // class specific funcs
   isTeleable(item) {
     return item.isTeleable
   }
 }
+
+/*
+notes
+  round?
+  steps
+    sub-steps
+  can be just generate steps, where some steps are automated mechanics executions, some steps are actions (user triggered actions)
+    user actions to be of same interface as mechanics, with forward(), backward(), generatePrevStep()
+*/
--- pzle.js
+++ pzle.js
@@ -6,6 +6,90 @@ class PzleGenerator {
   }
 }
 
+// data structures
+const controllableSampleV1 = {
+  id: 'p1',
+  // action data structure
+  action: {
+    type: 'nav',
+    data: {
+      dest: {
+        x:2, y:2
+      }
+    }
+  },
+  isTeleable: true,
+}
+const stateSampleV1 = {
+  // lists of diff major types of things
+  mechanics: [
+    {
+      type: 'tele',
+      teleIn: {
+        x:1, y:1,
+      },
+      // ...
+    }
+  ],
+  controllables: [
+    {
+      // ...
+    }
+  ],
+  env: {
+    // env of the pzle curr state
+  }
+}
+const pzleSampleV1 = {
+  /*
+  Path: poss path the gm may goes down, including the N diff solutions and M diff failures. Paths should be of tree structure, start at start state, diff user action executed lead to diff nextState and their next child tree of path
+    data needed for unty to build the pzle and for match to start:
+      actually only start state is needed. the whole path is only for showing that the pzle can be solved
+      testing should verify if pzle in unty can be played exactly as the path obj stated
+  */
+  pathV1: {
+    /*
+    controllable actions should simply be some facts and should not be distinguishly separated out?
+    */
+    state: {
+      // ...
+    },
+    nextSteps: [
+      {
+        controllableActions: [
+          // it can be N controllables do diff actions to results in this next step
+          {
+            controllable: c1,
+            action: a1,
+          },
+          // ...
+        ],
+        nextStep: {
+          state: {
+            // ...
+          },
+          nextSteps: [
+            {
+              // some next step are purely mechanics auto executed, their controllableActions will be empty arr. nextSteps in such case should also have 1 item only.
+              controllableActions: [],
+              nextStep: {
+                // ...
+              }
+            }
+            // ...
+          ]
+        }
+      },
+      // ...
+    ]
+  }
+}
 class Pzle {
 
 }
+
+/*
+have data structures of pzle, state, action, ... be well defined to know how the generators should be generating diff things
+generate from end state vs generate from start state
+  generate from end state can ensure gd end state, e.g. consistent end state with certain format / characteristics
+*/
